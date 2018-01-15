c2f62b15222e4e15134b913df9172264cab5aeec
cedwards14 <35229175+cedwards14@users.noreply.github.com>
2018-01-15 09:05:17 -0700

    commenting up my main.js

--- Week 02/JS/main.js
+++ Week 02/JS/main.js
@@ -2,6 +2,14 @@
 this is a comment block, enclosed in a slash+ asterisk
 and ending with an asterisk + slash */
 
-// Demo of callign a function 
+// Demo of callign a function - console is the javascript editor environment in the browser
+// console.log(message)
     
-Console.log('main.js is loaded');
+console.log('main.js is loaded');
+
+//The updateInnerHTML function has two parameters 
+// - selectror - is a string that identifies which DOM element to get or selector
+// -newValue is a string that will be the new value for the selectors .innerhtml
+function updateInnerHTML(selector, newValue) {
+  document.querySelector(selector).innerHTML = newValue;
+}
